36e56e72d9c9e3ea46ad02a5832a37d6dffef7aa
Nguyễn Xuân Tùng <tung.nx@teko.vn>
2021-05-14 14:09:02 +0700

    Initial Commit

---
+++ LoyaltyDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoyaltyDemo
+//
+//  Created by Tung Nguyen on 5/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
